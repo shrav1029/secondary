817c1f6e0dba33fbb6e10969a9edde86929fd9fb
shrav1029 <115097478+shrav1029@users.noreply.github.com>
2022-10-15 19:25:52 +0400

    Add files via upload

---
+++ javascript1.js
@@ -0,0 +1,91 @@
+const myimg2= new Image(100,100)
+myimg2.src='11.jpeg'
+const img3=new Image(150,150)
+var para=document.getElementById('p')
+var text=document.createTextNode('\n ')
+img3.src='Osama_bin_Laden_portrait.jpg'
+var count=0
+const myimg= new Image()
+myimg.src='9.jpeg';
+/*
+if (count%2==0) {
+    document.querySelector('html').addEventListener('click',() =>{ 
+    console.log('WEEEEEEEEEEEE! \'allahuakbar\'')
+    alert('a plane has hit the world trade centre ')
+    document.body.appendChild(myimg2);
+    document.body.appendChild(myimg);
+    document.body.appendChild(img3);
+    document.body.appendChild(text)  
+    alert('Now the page will go back to normal')
+    document.body.removeChild(myimg2);
+    document.body.removeChild(myimg);
+    document.body.removeChild(img3);
+    document.body.removeChild(text);  
+    count++
+    console.log('HELLO');
+})}else{
+    console.log('HELOEELEOLO')
+
+
+    why.addEventListener("click",function(){ 
+            console.log('WEEEEEEEEEEEE! \'allahuakbar\'')
+            alert('a plane has hit the world trade centre ')
+            document.body.appendChild(myimg2);
+            document.body.appendChild(myimg);
+            document.body.appendChild(img3);
+            document.body.appendChild(text)  })};
+            console.log('This works');
+}*/
+var x=document.getElementById('allthetext');
+var y=document.getElementById("btn");
+y.addEventListener('click',() =>{
+    myFunction()
+})
+document.addEventListener('click',() =>{
+    console.log('CLICKED')
+})
+function myFunction() {
+    setTimeout(() => {}, 10000);
+    console.log('IT FINALLY WORKS ! \'allahuakbar\'')
+    alert('This is happeneing.....Calm down ');
+    x.style.visibility='visible';
+    console.log('COMEONENENE');
+    count++
+    };
+function idkifthisworks(){
+    x.style.visibility='hidden'
+    console.log('SOMETHING HAS HAPPENED')
+}
+var z=btn2.addEventListener('click',()=>{
+    idkifthisworks()
+})
+var text1=document.getElementById('input').From
+console.log(text1)
+var k=monke.addEventListener('click',()=>{
+    console.log('monke')
+    allthetext.removeChild(document.getElementById('monke-pic'))
+    allthetext.removeChild(document.getElementById('monke'))
+})
+var k=luffy.addEventListener('click',()=>{
+    console.log('luffy')
+    allthetext.removeChild(document.getElementById('luffy-pic'))
+    allthetext.removeChild(document.getElementById('luffy'))
+})
+var k=gif.addEventListener('click',()=>{
+    console.log('the gif')
+    allthetext.removeChild(document.getElementById('gif-pic'))
+    allthetext.removeChild(document.getElementById('gif'))
+    caches.open('USERNAME').then()
+        cache =>{
+            cache.add(text1)
+
+        }
+    })
+
+/*
+function idkifthisworks(){
+            alert('Now the page will go back to normal')
+            count++
+            console.log('HELLO')
+            x.style.visibility='hidden'
+};*/
